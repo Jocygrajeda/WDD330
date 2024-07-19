768692109c21f613af383b1b586f48718b48eedc
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-07-19 14:33:16 -0500

    step 3

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
 
   build: {
     outDir: "../dist",
+    assetsDir: "public",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
