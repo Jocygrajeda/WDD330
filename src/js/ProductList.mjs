25b5df315480cf67018a7026238139291e16a9ea
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-07-21 12:48:44 -0500

    fixing errors

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,15 +3,13 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   return `<li class="product-card">
   <a href="/product_pages/index.html?product=${product.Id}">
-   <img
+  <img
     src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
-   />
+  />
   <h3 class="card__brand">${product.Brand.Name}</h3>
   <h2 class="card__name">${product.Name}</h2>
-  <span class="product-card__discount">$${product.SuggestedRetailPrice} </span>
-  <p class="product-card__price">$${product.FinalPrice}</p>
-  </a>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
 </li>`;
 }
 
@@ -20,16 +18,13 @@ export default class ProductList {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
-    this.products = [];
   }
-
   async init() {
     const list = await this.dataSource.getData(this.category);
     this.renderList(list);
+    document.querySelector(".title").innerHTML = this.category;
   }
-
   renderList(list) {
-    const htmlStrings = list.map(productCardTemplate);
-    this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
   }
 }
